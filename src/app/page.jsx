2330cca6ac8ebe72afc5f00a7df17e180e0c22f6
shashwatpandey100 <115147038+shashwatpandey100@users.noreply.github.com>
2023-12-30 18:22:24 +0530

    rereTestimonial deploy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Intro from "@/components/Intro";
 import SmoothScroll from "@/components/smoothScroll.js";
 import Featured from "@/components/Featured.jsx";
 import Latest from "@/components/Latest.jsx";
-import IntroVideo from "@/components/IntroVideo.jsx";
+// import IntroVideo from "@/components/IntroVideo.jsx";
 import Team from "@/components/Team";
 import Testimonials from "@/components/Testimonials/Testimonials";
 
@@ -36,7 +36,7 @@ export default function Home() {
           <Featured />
         </section>
         <section className="mb-[30px]">
-          <IntroVideo />
+          {/* <IntroVideo /> */}
         </section>
         <section>
           <Latest />
