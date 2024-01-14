8c1a2aa8422687fe96b04dfdc828d6442a9decca
shashwatpandey100 <115147038+shashwatpandey100@users.noreply.github.com>
2024-01-14 14:15:40 +0530

    agent link fixed

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,15 +5,15 @@ const Team = () => {
   return (
     <section className="min-h-[calc(100vh-90px)] mb-[50px] w-full bg-white grid md:grid-cols-3 grid-cols-2 box-border relative">
       <section className="h-full w-full bg-[rgba(0,0,0,0.55)] absolute top-0 left-0 flex flex-col items-center justify-center gap-[22px]">
-        <h3 className="text-[2.5em] font-[700] leading-[1]">Meet Our Team</h3>
+        <h3 className="text-[2.5em] font-[700] leading-[1]">Meet Our Agents</h3>
         <p className="text-center">
           We are passionate about our work and its impact, and excited <br />{" "}
           about the humans who want to build the future of authentication with
           us. Our team is <br /> shaped by a set of hiring values.
         </p>
-        <Link href='/team' className="px-[15px] py-[7px] rounded-[25px] border border-white flex gap-[15px] items-center justify-center cursor-pointer group hover:bg-white transition-all duration-300">
+        <Link href='/agents' className="px-[15px] py-[7px] rounded-[25px] border border-white flex gap-[15px] items-center justify-center cursor-pointer group hover:bg-white transition-all duration-300">
           <span className="bg-white group-hover:bg-black h-[10px] w-[10px] rounded-full transition-all duration-300"></span>
-          <span className="group-hover:text-black transition-all duration-300">Our Team</span>
+          <span className="group-hover:text-black transition-all duration-300">Our Agents</span>
         </Link>
       </section>
       <div
